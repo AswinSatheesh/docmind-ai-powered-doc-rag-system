5b2f5ff9a44224f095af1cb590b657f51192d481
AswinSatheesh <aswinsatheesh2001@gmail.com>
2026-07-11 16:11:02 +0530

    user module crud operation completed

--- src/main/java/com/docmind/controller/UserController.java
+++ src/main/java/com/docmind/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.docmind.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.docmind.dto.CreateUserRequest;
+import com.docmind.dto.UpdateUserRequest;
 import com.docmind.dto.UserResponse;
 import com.docmind.service.UserService;
 
@@ -28,5 +31,17 @@ public class UserController {
 	@GetMapping("/{id}")
 	public UserResponse getUserById(@PathVariable Long id) {
 		return userService.getUserById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteRecordById(@PathVariable Long id) {
+		userService.deleteRecordById(id);
+		
+		return "User with ID " + id + " has been successfully removed";
+	}
+	
+	@PutMapping("/{id}")
+	public UserResponse updateUserRequestById(@PathVariable Long id, @RequestBody UpdateUserRequest request) {
+		return userService.updateUserReqById(id,request);
 	}
 }
--- src/main/java/com/docmind/service/UserService.java
+++ src/main/java/com/docmind/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.docmind.dto.CreateUserRequest;
+import com.docmind.dto.UpdateUserRequest;
 import com.docmind.dto.UserResponse;
 import com.docmind.entity.User;
 import com.docmind.mapper.UserMapper;
@@ -28,10 +29,37 @@ public class UserService {
 		return UserMapper.toResponse(savedUser);
 	}
 	
+	//get userByID
 	public UserResponse getUserById(Long id) {
 		User userEntity = userRepository.findById(id)
 				.orElseThrow(()-> new RuntimeException("User Not Found by Id : " + id));
 		
 		return UserMapper.toResponse(userEntity);
+	}
+	
+	//delete userByID
+	public void deleteRecordById(Long id) {
+		if(userRepository.existsById(id)) {
+			userRepository.deleteById(id);
+		}else {
+			throw new RuntimeException("Cannot Delete. User not found with id : " + id);
+		}
+	}
+	
+	//update userRequest By Id : 
+	public UserResponse updateUserReqById(Long id, UpdateUserRequest request) {
+		
+		//1. Fetch the existing entity record row from PostgreSQL database
+		User existingUser = userRepository.findById(id).orElseThrow(()-> new RuntimeException("User With Id Not Found"));
+		
+		// 2. Overwrite changing attributes from our inbound DTO packet
+		existingUser.setEmail(request.getEmail());
+		existingUser.setName(request.getName());
+		
+		// 3. Save the modified row state back down to persistent disk storage
+		User updatedUser = userRepository.save(existingUser);
+		
+		// 4. Return translated safe response blueprint payload back to gateway
+		return UserMapper.toResponse(updatedUser);
 	}
 }
