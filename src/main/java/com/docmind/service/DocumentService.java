076e031b194f34b7aa233b7f97717d06f332cd81
AswinSatheesh <aswinsatheesh2001@gmail.com>
2026-07-13 13:20:45 +0530

    document file upload and metadata store in db

--- src/main/java/com/docmind/service/DocumentService.java
+++ src/main/java/com/docmind/service/DocumentService.java
@@ -1,5 +1,10 @@
 package com.docmind.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,19 +19,51 @@ import com.docmind.mapper.DocumentMapper;
 import com.docmind.repository.DocumentRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.var;
 
 @Service
-@RequiredArgsConstructor
 public class DocumentService {
 	private final DocumentRepository documentRepository;
 	private final DocumentMapper documentMapper; //We can access either like this Dependency Injection or by declaring that method as static and directly accessing classname.method
 	
+	private final Path rootLocation;
+	
+	// This constructor injects the database repository and initializes the file folder storage location
+	public DocumentService(DocumentRepository documentRepository, @org.springframework.beans.factory.annotation.Value("${file.upload-dir}") String uploadDir,DocumentMapper documentMapper) {
+		this.documentRepository = documentRepository;
+		this.rootLocation = Paths.get(uploadDir);
+		this.documentMapper = documentMapper;
+		
+		// Automatically build the target directories on your computer if they don't exist yet
+		try {
+			Files.createDirectory(this.rootLocation);
+		}catch(IOException e) {
+			throw new RuntimeException("Could not initialize storage Path : ", e);
+		}
+	}
+	
 	public DocumentResponse uploadDocument(MultipartFile file, Long userId) {
 		try {
 			
+			if(file.isEmpty()) {
+				throw new RuntimeException("Failed to save empty file");
+			}
+			
+			// 1. Generate a clean, safe filename string target path
+			
+			String originalFileName = file.getOriginalFilename();
+			//"Create the full path where the uploaded file should be stored."
+			Path destinationFile = this.rootLocation.resolve(Paths.get(originalFileName)).normalize();
+			
+			// 2. Physically copy the incoming network file stream bytes onto your local computer storage drive
+			try (var inputStream = file.getInputStream()){
+				Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+			}
+			
 			//create a fresh row entity instance
 			Document document = new Document();
-			document.setFileName(file.getOriginalFilename());
+			document.setFileName(originalFileName);
 			document.setFileSize(file.getSize());
 			document.setFileType(file.getContentType());
 			document.setUploadDate(LocalDateTime.now());
