a56d54e7a6526d83d0f6fa11d83c9fd6bdf9761b
AswinSatheesh <aswinsatheesh2001@gmail.com>
2026-07-15 09:53:59 +0530

    LLM giving response based on the question

--- src/main/java/com/docmind/controller/AiController.java
+++ src/main/java/com/docmind/controller/AiController.java
@@ -3,6 +3,7 @@ package com.docmind.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.docmind.service.AiService;
@@ -16,8 +17,13 @@ public class AiController {
 	private final AiService aiService; //dependency Injection
 	
 	// Expose a dynamic GET endpoint to trigger file summary generation
-	@GetMapping("/summarize/{documentId}")
-	public String getDocumentSummary(@PathVariable Long documentId) {
-		return aiService.summarizeDocument(documentId);
+//	@GetMapping("/summarize/{documentId}")
+//	public String getDocumentSummary(@PathVariable Long documentId) {
+//		return aiService.summarizeDocument(documentId);
+//	}
+	
+	@GetMapping("/chat/{documentId}")
+	public String chatWithDocument(@PathVariable Long documentId, @RequestParam String question, @RequestParam String sessionId) {
+		return aiService.chatWithDocument(documentId, question, sessionId);
 	}
 }
--- src/main/java/com/docmind/service/AiService.java
+++ src/main/java/com/docmind/service/AiService.java
@@ -9,6 +9,9 @@ import java.rmi.server.LoaderHandler;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,50 +23,69 @@ import com.docmind.repository.DocumentRepository;
 public class AiService {
 	private final DocumentRepository documentRepository;
 	private final Path rootLocation;
-	private final ChatModel chatModel;
+//	private final ChatModel chatModel;
+	private final ChatClient chatClient;
 	
-	// Injecting ChatModel lets Spring AI automatically connect to out Gemini Cloud API key!
-	public AiService(DocumentRepository documentRepository, ChatModel chatModel, @Value("${file.upload-dir}") String uploadDir) {
+	// Inject Spring AI's ChatMemory repository (which points to PostgreSQL via JDBC)
+	public AiService(DocumentRepository documentRepository,
+			ChatClient.Builder chatCilentBuilder,
+			ChatMemory chatMemory,
+			@Value("${file.upload-dir}") String uploadDir
+			) {
+		
 		this.documentRepository = documentRepository;
 		this.rootLocation = Paths.get(uploadDir);
-		this.chatModel = chatModel;
+		
+		// Build the ChatClient with the MessageChatMemoryAdvisor attached!
+		this.chatClient = chatCilentBuilder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
 	}
 	
-	//Read the file content from our local drive and transmit it to gemini for an intelligent summary
-	public String summarizeDocument(Long documentId) {
-		
-		//1.check for this documentId record available or not 
+	/**
+     * Ask a question about a document while maintaining conversation history
+     * * @param documentId The document to read
+     * @param userQuestion The user's query (e.g. "What is the main topic?")
+     * @param sessionId A unique identifier for the conversation session (e.g. "session-123")
+     */
+	
+	public String chatWithDocument(Long documentId,String userQuestion, String sessionId) {
 		Document document = documentRepository.findById(documentId)
-				.orElseThrow(()-> new RuntimeException("Record not found for Id : " + documentId));
-		
-		// 2. Point directly to where the physical file rests on your hard drive
-		Path physicalPath = rootLocation.resolve(document.getFileName()).normalize();
-		
-		String fileExtension = getFileExtension(document.getFileName()); //Method to extract file extension
+							.orElseThrow(()-> new RuntimeException("Document record not found with id : " + documentId));
+		Path physicalFilePath = this.rootLocation.resolve(document.getFileName()).normalize();
+		String fileExtension = getFileExtension(document.getFileName());
 		
 		try {
 			String documentContent;
-			
-			// Route processing based on file type
 			if("pdf".equalsIgnoreCase(fileExtension)) {
-				documentContent = extractTextFromPdf(physicalPath);
-			}else {				
-				// Extract the text lines out of the file //fallback as txt
-				documentContent = Files.readString(physicalPath);
+				documentContent = extractTextFromPdf(physicalFilePath);
+			}else {
+				documentContent = Files.readString(physicalFilePath);
 			}
 			
+			// System instructions telling the AI how to act and what document to refer to
+			String systemInstructions = """
+					You are DocMind, an expert document assistant.
+					Answer the user's questions using the document text below.
+					If the answer is not in the document, use your general knowledge but mention it's not in the file.
+					
+					DOCUMENT TEXT :
+					-------------------------
+					%s
+					-------------------------
+					""".formatted(documentContent);
 			
-			// 4. Formulate the precise instruction package for Gemini
-			String structuralPrompt = "You are an expert document analyst. Please read the following file" 
-									+ "and provide a concise, structured, bullet-point summary:\n\n"
-									+ documentContent;
-			// 5. Fire it up to the Gemini cloud server and extract the final text response string
-			return chatModel.call(structuralPrompt);
+			// Execute the chat request with the context of our sessionId
+			return chatClient.prompt()
+					.system(systemInstructions)
+					.user(userQuestion)
+					.advisors(advisorSpec -> advisorSpec.param("chat_memory_conversation_id",sessionId))
+					.call()
+					.content();
 			
 		}catch(IOException e) {
-			throw new RuntimeException("Error occurred while reading physical file from disk storage " + e);
+			throw new RuntimeException("Error Reading document file : " +e);
 		}
 	}
+	
 	
 	/**
      * Simple helper to extract the file extension (e.g., "pdf", "txt")
