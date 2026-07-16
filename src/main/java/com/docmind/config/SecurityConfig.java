10a4fb86feb9390613c4e355c32bb598f0bf5bf0
AswinSatheesh <aswinsatheesh2001@gmail.com>
2026-07-16 22:54:33 +0530

    cors Security configuration added

--- src/main/java/com/docmind/config/SecurityConfig.java
+++ src/main/java/com/docmind/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.docmind.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,13 +27,29 @@ public class SecurityConfig {
 	// 2. This bean controls which URLs are locked and which are publicly accessible
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf-> csrf.disable())// Disable CSRF for stateless REST APIs
+		http
+		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+		.csrf(csrf-> csrf.disable())// Disable CSRF for stateless REST APIs
 		.authorizeHttpRequests(auth -> auth
 		// Allow absolute public access to Swagger files and our user paths
 //		.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/users/**").permitAll()
 		.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/users/**","/documents/**","/ai/**").permitAll()
 		.anyRequest().authenticated());
 		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOriginPatterns(List.of("https://localhost:5173")); //react url
+		corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+		corsConfiguration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+		corsConfiguration.setExposedHeaders(List.of("Authorization"));
+		corsConfiguration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return source;
 	}
 	
 }
